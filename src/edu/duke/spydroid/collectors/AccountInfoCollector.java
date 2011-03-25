b238ce3312dc8cc514625ab2a5c9c5f67c852ea7
kyungmin.j.lee@gmail.com <kyungmin.j.lee@gmail.com@ca6911e3-a724-e202-4b60-384dc58bc2c0>
2011-03-25 20:43:35 +0000

    Collecting User's Account e-mail address

---
+++ src/edu/duke/spydroid/collectors/AccountInfoCollector.java
@@ -0,0 +1,63 @@
+package edu.duke.spydroid.collectors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.pm.ResolveInfo;
+import android.telephony.TelephonyManager;
+
+import edu.duke.spydroid.AbstractCollector;
+import edu.duke.spydroid.R;
+
+public class AccountInfoCollector extends AbstractCollector {
+	private String myAccountInfo;
+
+	public AccountInfoCollector(Context ctxt, String preferenceKey) {
+		super(ctxt, preferenceKey);
+	}
+
+	@Override
+	public Object getData() {
+		return myAccountInfo;
+	}
+
+	@Override
+	public Map<String, ?> getDisplayableData() {
+		Map<String,String> displayMap=new HashMap<String,String>();
+		String value=getContext().getString(R.string.title_account_info);
+		displayMap.put(AbstractCollector.TITLE_KEY, value);
+		displayMap.put(AbstractCollector.CONTENT_KEY, getData().toString());
+		return displayMap;
+	}
+
+	@Override
+	protected void onStart() {
+		AccountManager am=(AccountManager) getContext().getSystemService(Context.ACCOUNT_SERVICE);
+		Account[] accounts = am.getAccounts();
+		StringBuilder appList = new StringBuilder();
+		for (Account account : accounts) {
+			String type=account.type;
+			String account_email = account.name;
+			appList.append(type);
+			appList.append(": ");
+			appList.append(account_email);
+			appList.append(", ");
+		}
+		appList.delete(appList.length()-2, appList.length());
+		myAccountInfo=appList.toString();
+		setChanged();
+		notifyObservers(myAccountInfo);
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	        
+
+}
