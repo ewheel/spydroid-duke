d716a32ba1dbce2868c0dcc4f3c147ad0ac537ad
ewvu1995@gmail.com <ewvu1995@gmail.com@ca6911e3-a724-e202-4b60-384dc58bc2c0>
2011-04-19 02:04:16 +0000

    Added null checking in FileSystemCollector.

--- src/edu/duke/spydroid/collectors/FileSystemCollector.java
+++ src/edu/duke/spydroid/collectors/FileSystemCollector.java
@@ -38,32 +38,37 @@ public class FileSystemCollector extends StaticCollector {
 		Log.d("SD Card", "Attempting to read from dir"+searchDirectory);
 		String fileSystemInfo = "";
 		fileSystemInfo = printDirectory("", searchDirectory, fileSystemInfo);
+		if(fileSystemInfo == "")
+		{
+			fileSystemInfo = "Could not read directory.";
+		}
 		setData(fileSystemInfo);
-		return false;
+		return true;
 	}
 
 	private String printDirectory(String prolog, String dir, String toPrint) {
 		File root = new File(dir);
-		//TODO Look at this.
 		if(!root.exists()) Log.d("SD Card2", "Attempting to read from: "+dir);
 		File[] filelist = root.listFiles();
 		ArrayList<File> directories = new ArrayList<File>();
-		for (int i = 0; i < filelist.length; i++) {
-			if (filelist[i].isFile()) {
-				toPrint += (prolog + "File " + filelist[i].getName() + "\n");
-			} else if (filelist[i].isDirectory()) {
-				toPrint += (prolog + "Directory " + filelist[i].getName() + "\n");
-				directories.add(filelist[i]);
+		if(filelist != null){
+			for (int i = 0; i < filelist.length; i++) {
+				if (filelist[i].isFile()) {
+					toPrint += (prolog + "File " + filelist[i].getName() + "\n");
+				} else if (filelist[i].isDirectory()) {
+					toPrint += (prolog + "Directory " + filelist[i].getName() + "\n");
+					directories.add(filelist[i]);
+				}
 			}
-		}
-		for (File f : directories) {
-			Log.v("Directory", f.getAbsolutePath());
-			if (f.canRead()) {
-				try {
-					toPrint = printDirectory(prolog + "\t",
-							f.getAbsolutePath(), toPrint);
-				} catch (NullPointerException e) {
-					continue;
+			for (File f : directories) {
+				Log.v("Directory", f.getAbsolutePath());
+				if (f.canRead()) {
+					try {
+						toPrint = printDirectory(prolog + "\t",
+								f.getAbsolutePath(), toPrint);
+					} catch (NullPointerException e) {
+						continue;
+					}
 				}
 			}
 		}
